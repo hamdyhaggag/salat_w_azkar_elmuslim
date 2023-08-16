54748a2bc72ea2fec283a007083cc1dbf4b9ad78
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-08-16 16:43:40 +0300

    add CopyButton

---
+++ lib/models/copy_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CopyButton extends StatelessWidget {
+  final String textToCopy;
+
+  const CopyButton(this.textToCopy, {super.key});
+
+  void copyTextToClipboard() {
+    Clipboard.setData(ClipboardData(text: textToCopy));
+    Fluttertoast.showToast(
+      msg: 'تم النسخ ',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(FontAwesomeIcons.copy, color: Color(0xff1E5A83)),
+      onPressed: copyTextToClipboard,
+    );
+  }
+}
--- lib/models/custom_container.dart
+++ lib/models/custom_container.dart
@@ -1,6 +1,5 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:salat_w_azkar_elmuslim/models/copy_button.dart';
 
 class CustomContainer extends StatelessWidget {
   final String title;
@@ -24,34 +23,30 @@ class CustomContainer extends StatelessWidget {
         width: double.infinity,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(15),
-          child: MaterialButton(
-            onPressed: () => '',
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      log('message');
-                    },
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CopyButton(title),
+                Column(
+                  children: [
+                    SizedBox(
+                      width:
+                          double.infinity, // Make the container take full width
+                      child: Text(
+                        title,
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.rtl,
+                        style: const TextStyle(
+                          color: Color(0xFF1E5A83),
+                          fontSize: 25.0,
+                        ),
+                      ),
                     ),
-                  ),
-                  Text(
-                    title,
-                    textAlign: TextAlign.center,
-                    textDirection: TextDirection.rtl,
-                    style: const TextStyle(
-                      color: Color(0xFF1E5A83),
-                      fontSize: 25.0,
-                    ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
