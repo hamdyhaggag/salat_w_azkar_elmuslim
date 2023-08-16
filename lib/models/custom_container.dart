c2102e741c8a76b64980c821fea08ed269f4beca
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-08-16 19:05:08 +0300

    wrap with GestureDetector

--- lib/models/custom_container.dart
+++ lib/models/custom_container.dart
@@ -13,84 +13,87 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(
-            color: const Color(0xFF1E5A83),
+    return GestureDetector(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+              color: const Color(0xFF1E5A83),
+            ),
           ),
-        ),
-        width: double.infinity,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CopyButton(title),
-                Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        title,
-                        textAlign: TextAlign.center,
-                        textDirection: TextDirection.rtl,
-                        style: const TextStyle(
-                          color: Color(0xFF1E5A83),
-                          fontSize: 25.0,
+          width: double.infinity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CopyButton(title),
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          title,
+                          textAlign: TextAlign.center,
+                          textDirection: TextDirection.rtl,
+                          style: const TextStyle(
+                            color: Color(0xFF1E5A83),
+                            fontSize: 25.0,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    // const CustomDivider(),
-                    Stack(
-                      children: [
-                        // The Container
-                        Container(
-                          height: 2,
-                          width: 300,
-                          color: const Color(0xff1E5A83),
-                        ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // const CustomDivider(),
+                      Stack(
+                        children: [
+                          // The Container
+                          Container(
+                            height: 2,
+                            width: 300,
+                            color: const Color(0xff1E5A83),
+                          ),
 
-                        const Positioned(
-                          top: 10,
-                          left: (300 - 54) / 2,
-                          child: CircleAvatar(
-                            backgroundColor: Color(0xff1E5A83),
-                            radius: 27,
-                            child: Text(
-                              '3',
-                              style: TextStyle(
-                                fontFamily: 'Cairo',
-                                fontSize: 20,
-                                color: Colors.white,
+                          const Positioned(
+                            top: 10,
+                            left: (300 - 54) / 2,
+                            child: CircleAvatar(
+                              backgroundColor: Color(0xff1E5A83),
+                              radius: 27,
+                              child: Text(
+                                '3',
+                                style: TextStyle(
+                                  fontFamily: 'Cairo',
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
-                        ),
 
-                        const SizedBox(
-                          height: 70,
-                        ),
-                      ],
-                    ),
-
-                    const Text(
-                      'عدد التكرارات',
-                      style: TextStyle(
-                        color: Color(0xFF1E5A83),
-                        fontSize: 25.0,
+                          const SizedBox(
+                            height: 70,
+                          ),
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              ],
+
+                      const Text(
+                        'عدد التكرارات',
+                        style: TextStyle(
+                          color: Color(0xFF1E5A83),
+                          fontSize: 25.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
