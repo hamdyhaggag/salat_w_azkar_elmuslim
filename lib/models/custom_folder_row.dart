2aed346cf0df4731c57a4637df5a0eed6ebf847a
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-08-17 15:19:04 +0300

    finish Custom Folder Row

--- lib/models/custom_folder_row.dart
+++ lib/models/custom_folder_row.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salat_w_azkar_elmuslim/models/custom_divider.dart';
 
 class CustomFolderRow extends StatelessWidget {
   const CustomFolderRow({
@@ -10,26 +11,40 @@ class CustomFolderRow extends StatelessWidget {
   final String title;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 12, top: 12),
-      child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 30,
-            fontFamily: 'Cairo',
-            fontWeight: FontWeight.w500,
-            color: Color(0xff1E5A83),
-          ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 12, top: 12),
+          child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 30,
+                fontFamily: 'Cairo',
+                fontWeight: FontWeight.w500,
+                color: Color(0xff1E5A83),
+              ),
+            ),
+            const SizedBox(
+              width: 22,
+            ),
+            const Icon(
+              FontAwesomeIcons.fileLines,
+              color: Color(0xff1E5A83),
+            )
+          ]),
         ),
         const SizedBox(
-          width: 22,
+          height: 10,
         ),
-        const Icon(
-          FontAwesomeIcons.fileLines,
-          color: Color(0xff1E5A83),
-        )
-      ]),
+        Container(
+          height: 2,
+          width: 420,
+          color: colorWithOpacity,
+        ),
+      ],
     );
   }
 }
+
+Color colorWithOpacity = const Color(0xff1E5A83).withOpacity(0.2);
