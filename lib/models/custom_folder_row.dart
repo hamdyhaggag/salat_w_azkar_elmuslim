78026164bd071b71e1dfe4fb7d4e7cc21dce8fb2
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-08-21 09:26:08 +0300

    Removed Unused imports

--- lib/models/custom_folder_row.dart
+++ lib/models/custom_folder_row.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:salat_w_azkar_elmuslim/models/custom_divider.dart';
 
 class CustomFolderRow extends StatelessWidget {
   const CustomFolderRow({
