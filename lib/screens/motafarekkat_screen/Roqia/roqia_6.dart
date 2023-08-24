6e3322831dc34a8a9d1881b31486836021ac9f5c
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-08-24 15:12:07 +0300

    Finish Rokia azkar

--- lib/screens/motafarekkat_screen/Roqia/roqia_6.dart
+++ lib/screens/motafarekkat_screen/Roqia/roqia_6.dart
@@ -14,7 +14,11 @@ class Roqia6 extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: 15),
-            CustomContainer(title: '', maxValue: 1, initialCounterValue: 0),
+            CustomContainer(
+                title:
+                    'عَنْ أَبِي هُرَيْرَةَ رَضِيَ اللَّهُ عَنْهُ ، أَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ : " يَنْزِلُ رَبُّنَا تَبَارَكَ وَتَعَالَى كُلَّ لَيْلَةٍ إِلَى السَّمَاءِ الدُّنْيَا ، حِينَ يَبْقَى ثُلُثُ اللَّيْلِ الْآخِرُ ، يَقُولُ : مَنْ يَدْعُونِي فَأَسْتَجِيبَ لَهُ ؟ مَنْ يَسْأَلُنِي فَأُعْطِيَهُ ؟ مَنْ يَسْتَغْفِرُنِي فَأَغْفِرَ لَهُ ؟ " .',
+                maxValue: 1,
+                initialCounterValue: 0),
             SizedBox(height: 15),
           ],
         ),
