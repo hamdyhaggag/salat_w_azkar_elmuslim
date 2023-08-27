3e73de45b9d69fe83fab127f1da3adcb6c55e533
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-08-28 00:03:01 +0300

    add responevie package

---
+++ lib/core/app_text.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:salat_w_azkar_elmuslim/core/colors.dart';
+import 'package:salat_w_azkar_elmuslim/core/dimentions.dart';
+
+// ignore: must_be_immutable
+class AppText extends StatelessWidget {
+  final String text;
+  final FontWeight fontWeight;
+  Color? color;
+  final double size;
+  final TextAlign? align;
+  final TextDecoration? decoration;
+  final TextOverflow? overflow;
+  final int? maxLines;
+  final bool? softWrap;
+  final double? height;
+  final String? fontFamily;
+
+  AppText({
+    super.key,
+    required this.text,
+    this.size = 14,
+    this.color,
+    this.fontWeight = FontWeight.w100,
+    this.align,
+    this.decoration,
+    this.overflow,
+    this.maxLines,
+    this.height,
+    this.fontFamily,
+    this.softWrap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: align ?? TextAlign.start,
+      textScaleFactor: 1,
+      style: TextStyle(
+        color: color ?? AppColors.blackColor,
+        fontSize: size.toInt().font,
+        height: height != null ? height! / size : null,
+        decoration: decoration ?? TextDecoration.none,
+        decorationStyle: TextDecorationStyle.solid,
+        fontWeight: fontWeight,
+        fontFamily: fontFamily ?? "lotta",
+      ),
+      overflow: overflow ?? TextOverflow.ellipsis,
+      maxLines: maxLines,
+      softWrap: softWrap,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:salat_w_azkar_elmuslim/app_cubit/app_cubit.dart';
 import 'package:salat_w_azkar_elmuslim/core/cache_helper.dart';
 import 'package:salat_w_azkar_elmuslim/core/dio_helper.dart';
@@ -26,31 +27,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AppCubit()..getMyCurrentLocation(),
-      child: MaterialApp(
-        builder: (context, child) => ResponsiveWrapper.builder(child,
-            maxWidth: 1200,
-            minWidth: 450,
-            defaultScale: true,
-            breakpoints: [
-              const ResponsiveBreakpoint.resize(450, name: MOBILE),
-              const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-              const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-              const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-            ],
-            background: Container(color: const Color(0xFFFFFFFF))),
-        title: "Tafakkur - تَفكر",
-        theme: ThemeData(
-          tooltipTheme: const TooltipThemeData(
-              textStyle: TextStyle(
-                  fontSize: 30, color: Colors.white, fontFamily: 'Cairo')),
-          textTheme: const TextTheme(
-            titleMedium: TextStyle(fontSize: 25, fontFamily: 'Cairo'),
-            bodyMedium: TextStyle(fontSize: 30, fontFamily: 'Cairo'),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        child: MaterialApp(
+          builder: (context, child) => ResponsiveWrapper.builder(child,
+              maxWidth: 1200,
+              minWidth: 450,
+              defaultScale: true,
+              breakpoints: [
+                const ResponsiveBreakpoint.resize(450, name: MOBILE),
+                const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+              ],
+              background: Container(color: const Color(0xFFFFFFFF))),
+          title: "Tafakkur - تَفكر",
+          theme: ThemeData(
+            tooltipTheme: const TooltipThemeData(
+                textStyle: TextStyle(
+                    fontSize: 30, color: Colors.white, fontFamily: 'Cairo')),
+            textTheme: const TextTheme(
+              titleMedium: TextStyle(fontSize: 25, fontFamily: 'Cairo'),
+              bodyMedium: TextStyle(fontSize: 30, fontFamily: 'Cairo'),
+            ),
+            fontFamily: 'Cairo',
           ),
-          fontFamily: 'Cairo',
+          debugShowCheckedModeBanner: false,
+          home: const SplashScreen(),
         ),
-        debugShowCheckedModeBanner: false,
-        home: const SplashScreen(),
       ),
     );
   }
